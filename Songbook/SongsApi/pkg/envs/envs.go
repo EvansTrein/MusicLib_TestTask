36daf3dd336daa17f27d93fb4480f1fad91ea0b4
Evans <evanstrein@icloud.com>
2024-11-25 07:24:47 +0300

    fix

--- Songbook/SongsApi/pkg/envs/envs.go
+++ Songbook/SongsApi/pkg/envs/envs.go
@@ -1,5 +1,7 @@
 package envs
 
+import "os"
+
 // Хранение данных значений ENV
 var ServerEnvs Envs
 
@@ -16,25 +18,25 @@ type Envs struct {
 
 // / Инициализация значений ENV
 func LoadEnvs() error {
-	ServerEnvs.POSTGRES_USER = "evans"
-	ServerEnvs.POSTGRES_PASSWORD = "evans"
-	ServerEnvs.POSTGRES_PORT = "3010"
-	ServerEnvs.POSTGRES_NAME = "postgres"
-	ServerEnvs.POSTGRES_HOST = "localhost"
-	ServerEnvs.POSTGRES_USE_SSL = "disable"
+	// ServerEnvs.POSTGRES_USER = "evans"
+	// ServerEnvs.POSTGRES_PASSWORD = "evans"
+	// ServerEnvs.POSTGRES_PORT = "3010"
+	// ServerEnvs.POSTGRES_NAME = "postgres"
+	// ServerEnvs.POSTGRES_HOST = "localhost"
+	// ServerEnvs.POSTGRES_USE_SSL = "disable"
 
-	ServerEnvs.SONGBOOK_PORT = "3000"
+	// ServerEnvs.SONGBOOK_PORT = "3000"
 
 	// для запуска через докер, все, что выше нужно добавить в коммиты, все ниже - коммиты убрать и ипортировать пакет OS
 
-	// ServerEnvs.POSTGRES_USER = os.Getenv("POSTGRES_USER")
-	// ServerEnvs.POSTGRES_PASSWORD = os.Getenv("POSTGRES_PASSWORD")
-	// ServerEnvs.POSTGRES_PORT = os.Getenv("POSTGRES_PORT")
-	// ServerEnvs.POSTGRES_NAME = os.Getenv("POSTGRES_NAME")
-	// ServerEnvs.POSTGRES_HOST = os.Getenv("POSTGRES_HOST")
-	// ServerEnvs.POSTGRES_USE_SSL = os.Getenv("POSTGRES_USE_SSL")
+	ServerEnvs.POSTGRES_USER = os.Getenv("POSTGRES_USER")
+	ServerEnvs.POSTGRES_PASSWORD = os.Getenv("POSTGRES_PASSWORD")
+	ServerEnvs.POSTGRES_PORT = os.Getenv("POSTGRES_PORT")
+	ServerEnvs.POSTGRES_NAME = os.Getenv("POSTGRES_NAME")
+	ServerEnvs.POSTGRES_HOST = os.Getenv("POSTGRES_HOST")
+	ServerEnvs.POSTGRES_USE_SSL = os.Getenv("POSTGRES_USE_SSL")
 
-	// ServerEnvs.SONGBOOK_PORT = os.Getenv("SONGBOOK_PORT")
+	ServerEnvs.SONGBOOK_PORT = os.Getenv("SONGBOOK_PORT")
 
 	return nil
 }
